dc1e910baf10ae7a6910595bd763adaa27b20831
ssung <qkrtjdcjf124@gmail.com>
2022-03-21 01:08:02 +0900

    몇몇 UI변경

--- src/main/java/com/ssung/travelDiary/web/members/dto/MemberSaveRequestDto.java
+++ src/main/java/com/ssung/travelDiary/web/members/dto/MemberSaveRequestDto.java
@@ -13,16 +13,13 @@ import javax.validation.constraints.Pattern;
 @Getter @Setter
 public class MemberSaveRequestDto {
 
-    @NotBlank
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$")
     private String username;
 
-    @NotBlank
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}")
     private String password;
 
-    @NotBlank
-    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$")
     private String email;
 
     private String image;
---
+++ src/main/resources/static/js/sign.js
@@ -0,0 +1,27 @@
+var main = {
+    init : function() {
+        var _this = this;
+
+        $("#sign").on("click", function() {
+            _this.signCheck();
+        });
+    },
+
+    signCheck : function() {
+        var id = $("#id").val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: "/board/"+id+"/delete",
+            dataType: "json",
+            contentType: "application/json; charset=UTF-8",
+        }).done(function() {
+            alert("글이 삭제되었습니다.");
+//            window.location.href="/board/privateBoardList"
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        })
+    }
+};
+
+main.init();

e82b4fad8fab25fe5d3890ec15618f23ca481bcb
ssung <qkrtjdcjf124@gmail.com>
2022-05-10 22:16:32 +0900

    MemberController 테스트코드 작성 시작

--- src/test/java/com/ssung/travelDiary/web/members/MemberControllerTest.java
+++ src/test/java/com/ssung/travelDiary/web/members/MemberControllerTest.java
@@ -1,25 +1,44 @@
 package com.ssung.travelDiary.web.members;
 
+import com.ssung.travelDiary.service.members.MemberService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-
+@ExtendWith(MockitoExtension.class)
 class MemberControllerTest {
 
-    @Autowired
-    private MemberController memberController;
+    @InjectMocks
+    MemberController memberController;
 
-//    @Test
-//    public void 비밀번호_체크_성공() throws Exception {
-//        // given
-//        String password = "test";
-//        String password_check = "test";
-//
-//        // when
-//        memberController.
-//
-//        // then
-//    }
+    @Mock
+    MemberService memberService;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    MockMvc mockMvc;
+
+    private final String baseUrl = "/member";
+
+    @BeforeEach
+    void beforeEach() {
+        mockMvc = MockMvcBuilders.standaloneSetup(memberController).build();
+    }
+
+    @Test
+    void 회원가입_폼() throws Exception {
+        mockMvc.perform(get(baseUrl + "/"))
+                .andExpect(status().isOk());
+    }
 }

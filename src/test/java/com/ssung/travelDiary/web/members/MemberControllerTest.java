07254dcf7abcc8470cf6c9cbe1d846ae15eaf74f
ssung <qkrtjdcjf124@gmail.com>
2022-05-11 23:46:26 +0900

    MemberController 테스트코드 추가

--- src/test/java/com/ssung/travelDiary/web/members/MemberControllerTest.java
+++ src/test/java/com/ssung/travelDiary/web/members/MemberControllerTest.java
@@ -1,6 +1,9 @@
 package com.ssung.travelDiary.web.members;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssung.travelDiary.domain.members.Role;
 import com.ssung.travelDiary.service.members.MemberService;
+import com.ssung.travelDiary.web.members.dto.MemberSaveRequestDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,11 +12,16 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class MemberControllerTest {
@@ -26,6 +34,8 @@ class MemberControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    ObjectMapper objectMapper = new ObjectMapper();
 
     MockMvc mockMvc;
 
@@ -40,5 +50,46 @@ class MemberControllerTest {
     void 회원가입_폼() throws Exception {
         mockMvc.perform(get(baseUrl + "/"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void 회원가입() throws Exception {
+        // given\
+        MockMultipartFile multipartFile = new MockMultipartFile("image", "test.png", MediaType.IMAGE_PNG_VALUE, "test".getBytes());
+//        MemberSaveRequestDto requestDto = new MemberSaveRequestDto("username", 1, "password", "password", "email", null, Role.USER);
+
+//        given(memberService.sign(requestDto)).willReturn(1L);
+
+        // when, then
+        mockMvc.perform(multipart(baseUrl + '/')
+                        .file(multipartFile)
+//                        .content(objectMapper.writeValueAsString(requestDto)))
+                        .param("username", "username")
+                        .param("username_validation", "1")
+                        .param("password", "password123!")
+                        .param("password_check", "password123!")
+                        .param("email", "email@naver.com"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+
+//        verify(memberService).sign(requestDto);
+    }
+
+    @Test
+    void 회원가입_검증에러_발생() throws Exception {
+        // given
+        mockMvc.perform(post(baseUrl + "/")
+                        .param("username", "username")
+                        .param("username_validation", "0")
+                        .param("password", "password")
+                        .param("password_check", "password1")
+                        .param("email", "email"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("members/sign"))
+                .andDo(print());
+
+        // when
+
+        // then
     }
 }

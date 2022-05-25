723147b8e0954dfe58ef62a24450d3cdf04bf5aa
ssung <qkrtjdcjf124@gmail.com>
2022-05-26 00:33:52 +0900

    í잘못된 회원 조회 시 보여줄 에러페이지 생성

--- src/main/java/com/ssung/travelDiary/advice/ControllerAdvice.java
+++ src/main/java/com/ssung/travelDiary/advice/ControllerAdvice.java
@@ -1,19 +1,21 @@
 package com.ssung.travelDiary.advice;
 
-import com.ssung.travelDiary.exception.member.MemberUsernameAlreadyExistException;
+import com.ssung.travelDiary.exception.member.MemberNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @org.springframework.web.bind.annotation.ControllerAdvice
 class ControllerAdvice {
 
-//    @ExceptionHandler(MemberUsernameAlreadyExistException.class)
-//    @ResponseStatus(HttpStatus.CONFLICT)
-//    public String MemberUsernameAlreadyExistException(MemberUsernameAlreadyExistException e) {
-//
-//    }
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String MemberUsernameAlreadyExistException(MemberNotFoundException e,
+                                                      HttpServletRequest request) {
+        return "error/MemberNotFound";
+    }
 }

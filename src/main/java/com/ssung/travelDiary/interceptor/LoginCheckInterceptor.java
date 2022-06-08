912b0bdfa0648f83a9bba7efbdc48f6f1077e42d
ssung <qkrtjdcjf124@gmail.com>
2022-06-09 02:21:55 +0900

    refector: JSESSION생성 방지 refector: 이미지 출력 이름 변경 refector: 필요없는 로그, 파일 제거

--- src/main/java/com/ssung/travelDiary/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/ssung/travelDiary/interceptor/LoginCheckInterceptor.java
@@ -15,20 +15,20 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
 
-        log.info("인터셉터 요청 = {}", requestURI);
+//        log.info("인터셉터 요청 = {}", requestURI);
 
         HttpSession session = request.getSession(false);
 
         if (session == null) {
-            log.info("미인증 사용자 요청");
+//            log.info("미인증 사용자 요청");
             response.sendRedirect("/login?redirectURL=" + requestURI);
             return false;
         } else {
-            Iterator<String> stringIterator = session.getAttributeNames().asIterator();
-            while (stringIterator.hasNext()) {
-                String sessionName = stringIterator.next();
-                log.info("name = {} | sessionValue = {}", sessionName, session.getAttribute(sessionName));
-            }
+//            Iterator<String> stringIterator = session.getAttributeNames().asIterator();
+//            while (stringIterator.hasNext()) {
+//                String sessionName = stringIterator.next();
+//                log.info("name = {} | sessionValue = {}", sessionName, session.getAttribute(sessionName));
+//            }
         }
 
         return true;

6cb8611b8357f0b6bf81dabd114540f391a408c1
ssung <qkrtjdcjf124@gmail.com>
2022-06-15 18:10:50 +0900

    refector: service단 통합테스트 제거

--- src/test/java/com/ssung/travelDiary/handler/FileHandlerTest.java
+++ src/test/java/com/ssung/travelDiary/handler/FileHandlerTest.java
@@ -14,82 +14,82 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-@Transactional
+//@SpringBootTest
+//@Transactional
 class FileHandlerTest {
 
-    @Autowired
-    FileHandler fileHandler;
-
-    @Value("${file.dir}")
-    private String fileDir;
-
-    @Test
-    void 파일의_전체경로_가져오기() throws Exception {
-        // given
-        MockMultipartFile multiFile = createMultiFile();
-
-        // when
-        String fullPath = fileHandler.getFullPath(multiFile.getOriginalFilename());
-
-        // then
-        assertThat(fullPath).isEqualTo(fileDir + multiFile.getOriginalFilename());
-    }
-
-    @Test
-    void 단일_파일_저장() throws Exception {
-        // given
-        MockMultipartFile multiFile = createMultiFile();
-
-        // when
-        FileDto fileDto = fileHandler.storeFile(multiFile);
-
-        // then
-        assertThat(fileDto.getOriginalFileName()).isEqualTo(multiFile.getOriginalFilename());
-    }
-
-    @Test
-    void 단일_파일_저장_빈_값() throws Exception {
-        // given
-        MockMultipartFile emptyMultiFile = createEmptyMultiFile();
-
-        // when
-        FileDto fileDto = fileHandler.storeFile(emptyMultiFile);
-
-        // then
-        assertThat(fileDto).isNull();
-    }
-
-    @Test
-    void 다중_파일_저장() throws Exception {
-        // given
-        List<MultipartFile> multipartFiles = List.of(createMultiFile(), createMultiFile());
-
-        // when
-        List<FileDto> fileDtos = fileHandler.storeFiles(multipartFiles);
-
-        // then
-        assertThat(fileDtos.size()).isEqualTo(2);
-        assertThat(fileDtos.get(0).getOriginalFileName()).isEqualTo(multipartFiles.get(0).getOriginalFilename());
-    }
-
-    @Test
-    void 다중_파일_저장_빈_값() throws Exception {
-        // given
-        List<MultipartFile> multipartFiles = List.of(createEmptyMultiFile(), createEmptyMultiFile());
-
-        // when
-        List<FileDto> fileDtos = fileHandler.storeFiles(multipartFiles);
-
-        // then
-        assertThat(fileDtos.size()).isEqualTo(0);
-    }
-
-    private MockMultipartFile createMultiFile() {
-        return new MockMultipartFile("image1", "test.png", MediaType.IMAGE_PNG_VALUE, "test".getBytes());
-    }
-
-    private MockMultipartFile createEmptyMultiFile() {
-        return new MockMultipartFile("null", new byte[]{});
-    }
+//    @Autowired
+//    FileHandler fileHandler;
+//
+//    @Value("${file.dir}")
+//    private String fileDir;
+//
+//    @Test
+//    void 파일의_전체경로_가져오기() throws Exception {
+//        // given
+//        MockMultipartFile multiFile = createMultiFile();
+//
+//        // when
+//        String fullPath = fileHandler.getFullPath(multiFile.getOriginalFilename());
+//
+//        // then
+//        assertThat(fullPath).isEqualTo(fileDir + multiFile.getOriginalFilename());
+//    }
+//
+//    @Test
+//    void 단일_파일_저장() throws Exception {
+//        // given
+//        MockMultipartFile multiFile = createMultiFile();
+//
+//        // when
+//        FileDto fileDto = fileHandler.storeFile(multiFile);
+//
+//        // then
+//        assertThat(fileDto.getOriginalFileName()).isEqualTo(multiFile.getOriginalFilename());
+//    }
+//
+//    @Test
+//    void 단일_파일_저장_빈_값() throws Exception {
+//        // given
+//        MockMultipartFile emptyMultiFile = createEmptyMultiFile();
+//
+//        // when
+//        FileDto fileDto = fileHandler.storeFile(emptyMultiFile);
+//
+//        // then
+//        assertThat(fileDto).isNull();
+//    }
+//
+//    @Test
+//    void 다중_파일_저장() throws Exception {
+//        // given
+//        List<MultipartFile> multipartFiles = List.of(createMultiFile(), createMultiFile());
+//
+//        // when
+//        List<FileDto> fileDtos = fileHandler.storeFiles(multipartFiles);
+//
+//        // then
+//        assertThat(fileDtos.size()).isEqualTo(2);
+//        assertThat(fileDtos.get(0).getOriginalFileName()).isEqualTo(multipartFiles.get(0).getOriginalFilename());
+//    }
+//
+//    @Test
+//    void 다중_파일_저장_빈_값() throws Exception {
+//        // given
+//        List<MultipartFile> multipartFiles = List.of(createEmptyMultiFile(), createEmptyMultiFile());
+//
+//        // when
+//        List<FileDto> fileDtos = fileHandler.storeFiles(multipartFiles);
+//
+//        // then
+//        assertThat(fileDtos.size()).isEqualTo(0);
+//    }
+//
+//    private MockMultipartFile createMultiFile() {
+//        return new MockMultipartFile("image1", "test.png", MediaType.IMAGE_PNG_VALUE, "test".getBytes());
+//    }
+//
+//    private MockMultipartFile createEmptyMultiFile() {
+//        return new MockMultipartFile("null", new byte[]{});
+//    }
 }
--- src/test/java/com/ssung/travelDiary/service/share/ShareServiceTest.java
+++ src/test/java/com/ssung/travelDiary/service/share/ShareServiceTest.java
@@ -27,103 +27,102 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@SpringBootTest
-@Transactional
-//@Rollback(value = false)
+//@SpringBootTest
+//@Transactional
 class ShareServiceTest {
 
-    @Autowired MemberService memberService;
-    @Autowired BoardService boardService;
-    @Autowired ShareService shareService;
-
-    private Long memberId;
-
-    @Test
-    public void 공유폴더_저장() throws Exception {
-        // given
-        ShareResponseDto share = createShare();
-
-        // when
-        List<ShareBoard> shareBoard = share.getShareBoard();
-        List<ShareMember> shareMember = share.getShareMember();
-
-        // then
-        assertThat(share.getTitle()).isEqualTo("shareTitle");
-        assertThat(shareBoard.size()).isEqualTo(2);
-        assertThat(shareMember.size()).isEqualTo(2);
-        assertThat(shareBoard.get(0).getShare().getId()).isEqualTo(share.getId());
-        assertThat(shareMember.get(0).getShare().getId()).isEqualTo(share.getId());
-    }
-
-    @Test
-    void 공유폴더_리스트_출력() throws Exception {
-        // given
-        createShare();
-
-        // when
-        ShareListResponseDto responseDto = shareService.findList(memberId).get(0);
-
-        // then
-        assertThat(responseDto.getTitle()).isEqualTo("shareTitle");
-    }
-
-    @Test
-    void 공유폴더_리스트_출력_member없음() throws Exception {
-        // given
-        MockMultipartFile multipartFile = new MockMultipartFile("null", new byte[]{});
-        MemberSaveRequestDto dto = new MemberSaveRequestDto("username3", "password3", "email3", multipartFile, Role.USER);
-        Long findId = memberService.save(dto);
-
-        // when
-        List<ShareListResponseDto> list = shareService.findList(findId);
-
-        // then
-        assertThat(list.size()).isEqualTo(0);
-    }
-
-    @Test
-    void 공유된_게시글_전체조회() throws Exception {
-        // given
-        ShareResponseDto share = createShare();
-
-        // when
-        List<ShareBoardResponseDto> shareBoard = shareService.findShareBoard(share.getId());
-
-        // then
-        assertThat(shareBoard.size()).isEqualTo(2);
-        assertThat(shareBoard.get(0).getTitle()).isEqualTo("title");
-    }
-
-    private ShareResponseDto createShare() throws IOException {
-        MockMultipartFile multipartFile = new MockMultipartFile("null", new byte[]{});
-
-        MemberSaveRequestDto dto = new MemberSaveRequestDto("username", "password", "email", multipartFile, Role.USER);
-        MemberSaveRequestDto dto2 = new MemberSaveRequestDto("username2", "password2", "email2", multipartFile, Role.USER);
-        Long memberId = memberService.save(dto);
-        Long memberId2 = memberService.save(dto2);
-        this.memberId = memberId;
-
-        BoardSaveRequestDto boardSaveRequestDto = new BoardSaveRequestDto("title", "content", "location", new ArrayList<>(), LocalDate.now().toString());
-        BoardSaveRequestDto boardSaveRequestDto2 = new BoardSaveRequestDto("title2", "content2", "location2", new ArrayList<>(), LocalDate.now().toString());
-        BoardResponseDto board = boardService.save(boardSaveRequestDto, memberId);
-        BoardResponseDto board2 = boardService.save(boardSaveRequestDto2, memberId);
-
-        String title = "shareTitle";
-        String creator = "test";
-
-        ArrayList<Long> members = new ArrayList<>();
-        members.add(memberId2);
-
-        ArrayList<Long> boards = new ArrayList<>();
-        boards.add(board.getId());
-        boards.add(board2.getId());
-
-        ShareSaveRequestDto shareSaveRequestDto = new ShareSaveRequestDto(title, creator, members, boards);
-        ShareSaveRequestDto shareSaveRequestDto2 = new ShareSaveRequestDto(title, creator, members, boards);
-
-        ShareResponseDto share = shareService.save(shareSaveRequestDto, memberId);
-        shareService.save(shareSaveRequestDto2, memberId);
-
-        return share;
-    }
+//    @Autowired MemberService memberService;
+//    @Autowired BoardService boardService;
+//    @Autowired ShareService shareService;
+//
+//    private Long memberId;
+//
+//    @Test
+//    public void 공유폴더_저장() throws Exception {
+//        // given
+//        ShareResponseDto share = createShare();
+//
+//        // when
+//        List<ShareBoard> shareBoard = share.getShareBoard();
+//        List<ShareMember> shareMember = share.getShareMember();
+//
+//        // then
+//        assertThat(share.getTitle()).isEqualTo("shareTitle");
+//        assertThat(shareBoard.size()).isEqualTo(2);
+//        assertThat(shareMember.size()).isEqualTo(2);
+//        assertThat(shareBoard.get(0).getShare().getId()).isEqualTo(share.getId());
+//        assertThat(shareMember.get(0).getShare().getId()).isEqualTo(share.getId());
+//    }
+//
+//    @Test
+//    void 공유폴더_리스트_출력() throws Exception {
+//        // given
+//        createShare();
+//
+//        // when
+//        ShareListResponseDto responseDto = shareService.findList(memberId).get(0);
+//
+//        // then
+//        assertThat(responseDto.getTitle()).isEqualTo("shareTitle");
+//    }
+//
+//    @Test
+//    void 공유폴더_리스트_출력_member없음() throws Exception {
+//        // given
+//        MockMultipartFile multipartFile = new MockMultipartFile("null", new byte[]{});
+//        MemberSaveRequestDto dto = new MemberSaveRequestDto("username3", "password3", "email3", multipartFile, Role.USER);
+//        Long findId = memberService.save(dto);
+//
+//        // when
+//        List<ShareListResponseDto> list = shareService.findList(findId);
+//
+//        // then
+//        assertThat(list.size()).isEqualTo(0);
+//    }
+//
+//    @Test
+//    void 공유된_게시글_전체조회() throws Exception {
+//        // given
+//        ShareResponseDto share = createShare();
+//
+//        // when
+//        List<ShareBoardResponseDto> shareBoard = shareService.findShareBoard(share.getId());
+//
+//        // then
+//        assertThat(shareBoard.size()).isEqualTo(2);
+//        assertThat(shareBoard.get(0).getTitle()).isEqualTo("title");
+//    }
+//
+//    private ShareResponseDto createShare() throws IOException {
+//        MockMultipartFile multipartFile = new MockMultipartFile("null", new byte[]{});
+//
+//        MemberSaveRequestDto dto = new MemberSaveRequestDto("username", "password", "email", multipartFile, Role.USER);
+//        MemberSaveRequestDto dto2 = new MemberSaveRequestDto("username2", "password2", "email2", multipartFile, Role.USER);
+//        Long memberId = memberService.save(dto);
+//        Long memberId2 = memberService.save(dto2);
+//        this.memberId = memberId;
+//
+//        BoardSaveRequestDto boardSaveRequestDto = new BoardSaveRequestDto("title", "content", "location", new ArrayList<>(), LocalDate.now().toString());
+//        BoardSaveRequestDto boardSaveRequestDto2 = new BoardSaveRequestDto("title2", "content2", "location2", new ArrayList<>(), LocalDate.now().toString());
+//        BoardResponseDto board = boardService.save(boardSaveRequestDto, memberId);
+//        BoardResponseDto board2 = boardService.save(boardSaveRequestDto2, memberId);
+//
+//        String title = "shareTitle";
+//        String creator = "test";
+//
+//        ArrayList<Long> members = new ArrayList<>();
+//        members.add(memberId2);
+//
+//        ArrayList<Long> boards = new ArrayList<>();
+//        boards.add(board.getId());
+//        boards.add(board2.getId());
+//
+//        ShareSaveRequestDto shareSaveRequestDto = new ShareSaveRequestDto(title, creator, members, boards);
+//        ShareSaveRequestDto shareSaveRequestDto2 = new ShareSaveRequestDto(title, creator, members, boards);
+//
+//        ShareResponseDto share = shareService.save(shareSaveRequestDto, memberId);
+//        shareService.save(shareSaveRequestDto2, memberId);
+//
+//        return share;
+//    }
 }
